0f6c6e52ed959316ef635175ad30d2db2b2ae70c
Biancalaragon <125667139+Biancalaragon@users.noreply.github.com>
2024-05-10 11:38:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,10 @@
 const botoes= document.querySelectorAll (".botao");
-console.log (botoes)
+const textos = document.querySelectorAll (".aba-conteudo");
 
-for (let i=0; i < botoes.length; i++){
+for (let i=0; i < botoes.length
+    ; i++){
     botoes[i].onclick = function () {
         for (let j=0; j<botoes.length;j++){
-            botoes[j].classList.remove("ativo");
-        }
-        botoes[i].classList.add("ativo");
-    }
-}
-const textos = document.querySelectorAll(".aba-conteudo");
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
-        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
@@ -20,3 +12,8 @@ for (let i = 0; i < botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
+const contadores = document.querySelectorAll (".contador");
+const tempoObjetivo1 = new Date ("2024-10-05T00:00:00");
+let tempoAtual = new Date ();
+
+contadores[0].textContent = tempoobjetivo1 - tempoAtual;
